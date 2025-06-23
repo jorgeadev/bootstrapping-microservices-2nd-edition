3ee69ea4adefa2b0f2556f8c941ca28494fa99b8
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2025-06-23 13:01:16 -0400

    Add commands to run the app to README.md file

--- chapter-02/example-01/src/index.js
+++ chapter-02/example-01/src/index.js
@@ -1,11 +1,27 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+console.log(process.env.PORT);
+
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
+});
+
+app.get("/video", async (req, res) => {
+	const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+	const stats = await fs.promises.stat(videoPath);
+	
+	res.writeHead(200, {
+		'Content-Type': 'video/mp4',
+		'Content-Length': stats.size,
+		'Accept-Ranges': 'bytes'
+	});
+	
+	fs.createReadStream(videoPath).pipe(res);
 });
 
 app.listen(PORT, () => {
